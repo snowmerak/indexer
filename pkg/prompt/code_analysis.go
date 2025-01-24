87c3b1a605c7b606f1e18848b3371f551718d7c8
snowmerak <snowmerak@outlook.com>
2025-01-24 16:25:44 +0900

    change prompt

--- pkg/prompt/code_analysis.go
+++ pkg/prompt/code_analysis.go
@@ -3,6 +3,7 @@ package prompt
 import (
 	"bytes"
 	"fmt"
+	"strings"
 
 	"github.com/nao1215/markdown"
 )
@@ -11,15 +12,27 @@ func CodeAnalysis(code string) (string, error) {
 	builder := new(bytes.Buffer)
 	md := markdown.NewMarkdown(builder).
 		H1("Code Analysis").
-		H2("Description").
-		PlainText("I want you to work as a senior software engineer at our company. Your work is code analysis our code and make summary.").
+		H2("System").
+		PlainText("You are a code analyzer. Your work is generating a description of a code block.").
 		H2("Code Block").
 		CodeBlocks("go", code).
 		H2("Requirement").
-		PlainText("Please write a summary of the code block above. The first 100 characters are required. The summary should be written in <summary> tag.")
+		PlainText("Write a description of the code block above. The description must be written in '## description' block. You must not make other contents except for the description.")
 	if err := md.Build(); err != nil {
 		return "", fmt.Errorf("failed to build markdown: %w", err)
 	}
 
 	return builder.String(), nil
+}
+
+func UnwrapCodeAnalysis(response string) string {
+	s := strings.Index(response, "## description")
+	if s == -1 {
+		s = strings.Index(response, "## Description")
+		if s == -1 {
+			return response
+		}
+	}
+
+	return strings.TrimSpace(response[s+len("## description"):])
 }
