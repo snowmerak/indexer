7e84af8c99ee36804ada9d1574058acf64cab2ea
snowmerak <snowmerak@outlook.com>
2025-01-25 01:33:26 +0900

    change create, gets, deletes signature

--- lib/store/code/code.go
+++ lib/store/code/code.go
@@ -11,11 +11,11 @@ type Data struct {
 }
 
 type Store interface {
-	Create(ctx context.Context)
+	Create(ctx context.Context) error
 	Save(ctx context.Context, id int, codeBlock string, filePath string, line int, description string) error
 	Get(ctx context.Context, id int) (*Data, error)
-	Gets(ctx context.Context, ids []int) ([]*Data, error)
+	Gets(ctx context.Context, ids ...int) ([]*Data, error)
 	Delete(ctx context.Context, id int) error
-	Deletes(ctx context.Context, ids []int) error
+	Deletes(ctx context.Context, ids ...int) error
 	Drop(ctx context.Context) error
 }
