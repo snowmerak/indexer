4766c87db4692940017f7513be18b73491e65f28
snowmerak <snowmerak@outlook.com>
2025-01-24 17:10:02 +0900

    make code store interface

---
+++ lib/store/code/code.go
@@ -0,0 +1,21 @@
+package code
+
+import "context"
+
+type Data struct {
+	Id          int
+	CodeBlock   string
+	FilePath    string
+	Line        int
+	Description string
+}
+
+type Store interface {
+	Create(ctx context.Context)
+	Save(ctx context.Context, id int, codeBlock string, filePath string, line int, description string) error
+	Get(ctx context.Context, id int) (*Data, error)
+	Gets(ctx context.Context, ids []int) ([]*Data, error)
+	Delete(ctx context.Context, id int) error
+	Deletes(ctx context.Context, ids []int) error
+	Drop(ctx context.Context) error
+}
