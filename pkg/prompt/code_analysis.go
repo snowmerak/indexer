e390be3e861764ef3c2ecbbdebb12ded52fcc6b6
snowmerak <snowmerak@outlook.com>
2025-01-27 03:26:10 +0900

    update prompt

--- pkg/prompt/code_analysis.go
+++ pkg/prompt/code_analysis.go
@@ -7,29 +7,31 @@ import (
 	"github.com/nao1215/markdown"
 )
 
-func CodeAnalysis(code string) string {
+func CodeAnalysis(language string, code string) string {
 	builder := new(bytes.Buffer)
 	md := markdown.NewMarkdown(builder).
 		H1("Code Analysis").
-		H2("System").
-		PlainText("You are a code analyzer. Your work is generating a explanation of a code block.").
+		H2("System Prompt").
+		PlainText("I want you to act as senior software engineer who make review and documentation.").
 		H2("Code Block").
-		CodeBlocks("go", code).
+		CodeBlocks(markdown.SyntaxHighlight(language), code).
 		H2("Requirement").
-		PlainText("Write a explanation of the code block above. The explanation must be written in '## explanation' block. You must not make other contents except for the explanation.")
+		PlainText(`Analyze the following code and provide a detailed, step-by-step explanation of its behavior.
+Specifically, identify each variable used in the code, describe its role, data type (if discernible), and track how its value changes during execution.
+Focus on explaining how the code actually works, rather than just its structure.`)
 	_ = md.Build()
 
 	return UnwrapCodeAnalysis(md.String())
 }
 
 func UnwrapCodeAnalysis(response string) string {
-	s := strings.Index(response, "## explanation")
+	s := strings.Index(response, "## description")
 	if s == -1 {
-		s = strings.Index(response, "## Explanation")
+		s = strings.Index(response, "## Description")
 		if s == -1 {
 			return response
 		}
 	}
 
-	return strings.TrimSpace(response[s+len("## explanation"):])
+	return strings.TrimSpace(response[s+len("## description"):])
 }
