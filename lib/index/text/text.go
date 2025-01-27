0e79c0d8baf3090e0307132fa99f2dcfc6af5e51
snowmerak <snowmerak@outlook.com>
2025-01-27 15:22:05 +0900

    make text interface

---
+++ lib/index/text/text.go
@@ -0,0 +1,26 @@
+package text
+
+import (
+	"context"
+)
+
+type Result[T any] struct {
+	Data  Data[T] `json:"data"`
+	Score float64 `json:"score"`
+}
+
+type SearchOption struct {
+	Limit          int
+	Offset         int
+	Page           int
+	Sort           []string
+	ScoreThreshold float64
+}
+
+type Text[T any] interface {
+	Create(ctx context.Context) error
+	Store(ctx context.Context, id int, payload T) error
+	Query(ctx context.Context, query string, option SearchOption) ([]Result[T], error)
+	Delete(ctx context.Context, id int) error
+	Drop(ctx context.Context) error
+}
